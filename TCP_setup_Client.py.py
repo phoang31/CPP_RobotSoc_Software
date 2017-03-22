83dd1da554f0a4e00aa048b43df6ddae4a84e1c7
phoang31 <phoang31@gmail.com>
2017-03-22 14:28:29 -0700

    Add files via upload

--- TCP_setup_Client.py.py
+++ TCP_setup_Client.py.py
@@ -1,53 +1,90 @@
 #Socket client example in python
  
-import socket                   #for sockets
-import sys                      #for exit
- 
-#create an INET, STREAMing socket
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #creating socket
-except socket.error:
-    print 'Failed to create socket'
-    sys.exit()
+import socket                                                                            #need this for tcpip set up
+import sys                                                                               #system specific parameters and functions. usually use to exit the program
+
+def tcpClient(IP, Portnum):
+    #Existing Message
+    ExitMessage = 'Quitting on User Request\n'
+
+    #Validating IP address
+    print("Testing IP address")
+    valid = 0
+    while(valid == 0):
+        try:
+            socket.inet_aton(IP)
+            valid = 1;
+            print("The IP address is Valid")
+        except socket.error:
+            print("Invalid IP address. Please enter a valid IP address:\n")
+            IP = raw_input()
+            valid = 0;      
+    #Validating port number
+    while(Portnum < 0 | Portnum > 65536):
+        print("Invalid Port Number. Please enter a valid port number:\n")
+        Portnum = raw_input()
+        
+    #create an INET, STREAMing socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                               
+    except socket.error:
+        print 'Failed to create socket'
+        sys.exit() #exit from python
      
-print 'Socket Created'
+    print 'Socket Created'
  
-host = '192.168.2.7';                               #host can be: IP address
-port = 5002;
+    host = IP;                                                                  #host can be: IP address
+    port = Portnum;             
 
     
-try:
-    remote_ip = socket.gethostbyname( host )        #get host name
+    try:
+        remote_ip = socket.gethostbyname( host )                                            #get host name
  
-except socket.gaierror:
-    #could not resolve
-    print 'Hostname could not be resolved. Exiting'
-    sys.exit()
+    except socket.gaierror:
+        #could not resolve
+        print 'Hostname could not be resolved. Exiting'
+        sys.exit()
  
-#Connect to remote server
-s.connect((remote_ip , port))                       #connect to the host server
+    #Connect to remote server
+    s.connect((remote_ip , port))                                                           #connect to the host server
 
-while 1:     
-    print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+    while 1:     
+        print 'Socket Connected to ' + host + ' on ip ' + remote_ip
  
-    #Send some data to remote server
+        #Send some data to remote server
 
-    i = 0
-    while i < 4:                        #number of packet send right now is 4
-        try :
-            print("Enter a command: F,B,R,L, and a nuumber for duration: ")
-            command = raw_input()
-            s.sendall(command)
-            print(i)
-        except socket.error:
-            print 'SendFailed'
-            sys.exit()
-        i = i + 1
+        i = 0
+        while i < 4:   #this while loop is for testing purposes                                                                     #number of packets send right now is 4
+            try :
+                print("Enter a command: d,b,r,l, and a number for duration: ")
+                command = raw_input()
+                if(command == 'q'):
+                    s.sendall(ExitMessage)
+                    print(ExitMessage)
+                    break
+                elif(command == 'Q'):
+                    s.sendall(ExitMessage)
+                    print(ExitMessage)
+                    break
+                else:
+                    s.sendall(command)
+                print(i)
+            except socket.error:
+                print 'SendFailed'
+                sys.exit()
+            i = i + 1
 
-    print 'Message send successfully'
+        print 'Message sent successfully'
      
-    #Now receive data
-   # reply = s.recv(4096)                #use s.recv() to receive data
+        #Now receive data
+       # reply = s.recv(4096)                #use s.recv() to receive data
      
-   # print reply
-    #s.close()                          
+       # print reply
+    s.close()                          
+
+print("Welcome to TCP Client\n")
+print("Please provide the IP address of the Host: ")
+ip = raw_input()
+print("Please enter a port number: ")
+portn = raw_input()
+tcpClient(ip, int(portn))
