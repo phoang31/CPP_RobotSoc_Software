3f45d3097b827436cab27b79cdf4728b8c124a71
Robert Marosi <rmarosi1@gmail.com>
2017-04-14 16:47:20 -0700

    Camera calibration and marker axes working

--- CameraCalibration.cpp
+++ CameraCalibration.cpp
@@ -11,7 +11,8 @@ const int FPS = 30;
 const int WAITKEY_DELAY_MS = 1000 / FPS;
 const int CAMERA_INDEX = 0;//webcamera. 1==external camera(usually)
 const float CALIB_SQUARE_LENGTH = 0.026f;//2.6cm
-//const float ARUCO_SQUARE_LENGTH = //TBD
+//const float ARUCO_SQUARE_LENGTH = 0.076f;//7.6cm marker side length
+const string CALIB_CONSTANTS_FILEPATH = "Camera_calibration_constants.txt";
 
 //get known positions of corners on calibration chessboard
 void generateKnownBoardPositions(Size boardSize, float squareEdgeLength, vector<Point3f>& corners)
@@ -52,6 +53,73 @@ void getChessboardCorners(vector<Mat> images, vector<vector<Point2f>>& allFoundC
 	}
 }
 
+//calculates camera matrix and distortion vector from calibration images
+void cameraCalibration(vector<Mat> calibrationImages, Size boardSize, float squareSideLength,
+	Mat& CameraMatrix, Mat& distortionMatrix)
+{
+	//coordinates detected on calibration images
+	vector<vector<Point2f>> checkerBoardImageSpacePoints;
+	getChessboardCorners(calibrationImages, checkerBoardImageSpacePoints, false);
+
+	//create known board positions for all images
+	vector<vector<Point3f>> worldSpaceCornerPoints(1);
+
+	generateKnownBoardPositions(boardSize, squareSideLength, worldSpaceCornerPoints[0]);
+	//resize matrix and copy values over
+	worldSpaceCornerPoints.resize(checkerBoardImageSpacePoints.size(), worldSpaceCornerPoints[0]);
+
+	//radial and tangential vectors
+	vector<Mat> rVectors, tVectors;
+	//distortion coefficients
+	distortionMatrix = Mat::zeros(8, 1, CV_64F);
+
+	//calibrate the camera
+	calibrateCamera(worldSpaceCornerPoints, checkerBoardImageSpacePoints, boardSize,
+		CameraMatrix, distortionMatrix, rVectors, tVectors);
+
+
+
+}
+
+//writes calibration constants to a text file
+bool saveCameraCalibration(string name, Mat cameraMatrix, Mat distortionCoefficients)
+{
+	ofstream outstream(name);//create output file stream
+	if(outstream)
+	{
+		//write 
+		uint16_t rows = cameraMatrix.rows;
+		uint16_t cols = cameraMatrix.cols;
+
+		for (int r = 0; r < rows; r++)
+		{
+			for (int c = 0; c < cols; c++)
+			{
+				double value = cameraMatrix.at<double>(r, c);
+				outstream << value << endl;//writing matrix elements to file
+			}
+		}
+
+		rows = distortionCoefficients.rows;
+		cols = distortionCoefficients.cols;
+
+		for (int r = 0; r < rows; r++)
+		{
+			for (int c = 0; c < cols; c++)
+			{
+				double value = distortionCoefficients.at<double>(r, c);
+				outstream << value << endl;//writing matrix elements to file
+			}
+		}
+		outstream.close();
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 int main()
 {
 	char keyPress;
@@ -62,8 +130,6 @@ int main()
 	Mat distortionMatrix;
 
 	vector<Mat> savedImages;//for storing good calibration images
-
-	vector<vector<Point2f>> markerCorners, rejectedCandidates;
 
 	//attempt to open the capture device. return if failure
 	VideoCapture cap(CAMERA_INDEX);
@@ -100,14 +166,57 @@ int main()
 		{
 			imshow("Camera feed", frame);
 		}
-		/*/////////////////////////////////////////////*/
 
+		//handle keypresses
 		keyPress = (char)waitKey(WAITKEY_DELAY_MS);
-		if (keyPress == 27)
+		switch (keyPress)
 		{
-			break;//escape key pressed
-		}
+		case ' '://spacebar saves image when we have a good frame
+			if (found)
+			{
+				Mat tempFrame;
+				frame.copyTo(tempFrame);
+				savedImages.push_back(tempFrame);
+				cout << "Saved image: " << savedImages.size() << endl;
+			}
+			else
+			{
+				cout << "Chessboard not found in image!" << endl;
+			}
+			break;
+		case 13://enter key begins calibration once we have enough images
+			if (savedImages.size() >= 15)
+			{
+				cout << "Calculating calibration constants. Please wait." << endl;
+				cameraCalibration(savedImages, Size(9, 6), CALIB_SQUARE_LENGTH, cameraMatrix, distortionMatrix);
+				cout << "Camera matrix = " << endl << cameraMatrix << endl;
+				cout << "Distortion coefficients = " << endl << distortionMatrix << endl;
+				bool savedResult = saveCameraCalibration(CALIB_CONSTANTS_FILEPATH, cameraMatrix, distortionMatrix);
+				if (savedResult == true)
+				{
+					cout << "Saved camera calibration constants to text file." << endl;
+				}
+				else
+				{
+					cout << "Failed to save calibration constants to text file!" << endl;
+				}
+			}
+			else
+			{
+				cout << "Not enough images for calibration. Need at least 15." << endl;
+			}
+			return 0;
+			break;
+		case 27://escape key exits program
+			cout << "Exiting" << endl;
+			return 0;
+			break;
+		default://do nothing for other keypresses
+			break;
+
+		};
 	}//capture device closes automatically on exit. no need to manually close it
+	 /*/////////////////////////////////////////////*/
     return 0;
 }
 
