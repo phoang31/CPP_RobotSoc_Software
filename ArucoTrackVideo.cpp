d49e9e9b85860ba56f490be77da0dda4948d1fd8
Robert Marosi <rmarosi1@gmail.com>
2017-05-05 15:46:06 -0700

    cleaned tracking code

--- ArucoTrackVideo.cpp
+++ ArucoTrackVideo.cpp
@@ -10,41 +10,46 @@ using namespace aruco;
 const int FPS = 30;
 const int WAITKEY_DELAY_MS = 1000 / FPS;
 const int CAMERA_INDEX = 1;//webcamera. 1==external camera(usually)
-const float ARUCO_SQUARE_LENGTH = 0.076f;//7.6cm marker side length
 const string CALIB_CONSTANTS_FILEPATH = "Camera_calibration_constants_projectCamera.txt";
 const int CORNER_MARKER_ID_0 = 0;
 const int CORNER_MARKER_ID_1 = 1;
 const int ROBOT_MARKER_ID = 2;
 const int MAX_PATH_BUFFER_LENGTH = 20;//must be greater than or equal to 2
 
-const String CONTROLS_WINDOW_NAME = "Ball Tracking Controls";
+//const String CONTROLS_WINDOW_NAME = "Ball Tracking Controls";
 const String CAMERA_WINDOW_NAME = "Camera Feed";
+/*
 const int CANNY_UPPER_THRESH_MAX = 255;
 const int CENTER_THRESH_MAX = 100;
 const int BALL_MIN_RADIUS_MAX = 100;
 const int BALL_MAX_RADIUS_MAX = 100;
 const int BLUR_KERNEL_RADIUS_MAX = 10;
+*/
 
 void calculateMarkerCenterAndAngle(vector<int> markerIDs, vector<vector<Point2f>> markerCorners, vector<Point2f> &markerCenters, vector<float> &angles);
-int startWebcamMonitoring(const Mat cameraMatrix, const Mat distortionCoeffs, float arucoSquareSideLength);
+Mat trackObjects(Mat grabbedFrame, const Ptr<DetectorParameters> detectorParameters, const Mat cameraMatrix, const Mat distortionCoeffs,
+	const Ptr<Dictionary> markerDictionary, Point2f &ballLocation, Point2f &robotLocation, float &robotOrientation, vector<Point2f> &fieldCorners,
+	deque<Point2f> &ballLocationsBuffer, deque<Point2f> &robotLocationsBuffer);
 bool readCameraCalibration(string fname, Mat& cameraMatrix, Mat& distortionCoefficients);
 vector<Point2f> drawFieldCorners(Mat frame, vector<int> markerIDs, vector<Point2f> markerCenters);
 int findRobotIndex(vector<int> markerIDs);
-void drawPath(Mat frame, Point2f newLocation, deque<Point2f> &locationsBuffer);
-Point2f trackCircles(Mat frame, int &cannyUpperThreshValue,
+void drawPath(Mat &frame, Point2f newLocation, deque<Point2f> &locationsBuffer, Scalar lineColor);
+Point2f trackCircles(Mat &frame, Mat &gray, int &cannyUpperThreshValue,
 	int &centerDetectionThreshValue, int &minBallRadiusValue, int &maxBallRadiusValue, int &blurKernelRadiusValue);
 
-//search through frames for aruco markers and draw orientation axes
-int startWebcamMonitoring(const Mat cameraMatrix, const Mat distortionCoeffs, float arucoSquareSideLength)
+//search through frames for aruco markers/ball and draw orientation axes. returns drawn-on frame, position of ball/robot and orientation of robot
+Mat trackObjects(Mat grabbedFrame, const Ptr<DetectorParameters> detectorParameters, const Mat cameraMatrix, const Mat distortionCoeffs,
+	const Ptr<Dictionary> markerDictionary, Point2f &ballLocation, Point2f &robotLocation, float &robotOrientation, vector<Point2f> &fieldCorners,
+	deque<Point2f> &ballLocationsBuffer, deque<Point2f> &robotLocationsBuffer)
 {
-	Mat grabbed, frame, gray;//frame to search for aruco markers and ball
-	vector<Point2f> markerCenters, fieldCorners;
-	Point2f circleCenter;
-	vector<float> markerThetas;
-	vector<int> markerIDs;
+	Mat frame, gray;//frame to search for aruco markers and ball
+
+	vector<Point2f> markerCenters;
 	vector<vector<Point2f>> markerCorners, rejectedCandidates;
+	vector<float> markerThetas;
 	vector<Vec3f> circles;
-	deque<Point2f> robotLocationsBuffer, ballLocationsBuffer;
+	vector<int> markerIDs;
+
 	int robotIndex;
 	int cannyUpperThreshValue = 100;
 	int centerDetectionThreshValue = 24;
@@ -52,6 +57,7 @@ int startWebcamMonitoring(const Mat cameraMatrix, const Mat distortionCoeffs, fl
 	int maxBallRadiusValue = 15;
 	int blurKernelRadiusValue = 1;
 
+	/*
 	//control window
 	namedWindow(CONTROLS_WINDOW_NAME, WINDOW_AUTOSIZE); // Create Window for trackbar
 	createTrackbar("Canny Thresh.", CONTROLS_WINDOW_NAME, &cannyUpperThreshValue, CANNY_UPPER_THRESH_MAX);
@@ -59,74 +65,50 @@ int startWebcamMonitoring(const Mat cameraMatrix, const Mat distortionCoeffs, fl
 	createTrackbar("Ball min. radius", CONTROLS_WINDOW_NAME, &minBallRadiusValue, BALL_MIN_RADIUS_MAX);
 	createTrackbar("Ball max. radius", CONTROLS_WINDOW_NAME, &maxBallRadiusValue, BALL_MAX_RADIUS_MAX);
 	createTrackbar("Blur kernel radius", CONTROLS_WINDOW_NAME, &blurKernelRadiusValue, BLUR_KERNEL_RADIUS_MAX);
+	*/
+																
+	undistort(grabbedFrame, frame, cameraMatrix, distortionCoeffs);//correct lens distortion on captured frame
 
-	//marker tracking
-	Ptr<DetectorParameters> parameters = DetectorParameters::create();			//using default detector parameters for now
+	detectMarkers(frame, markerDictionary, markerCorners, markerIDs, detectorParameters);//detect Aruco markers in the frame
 
-	Ptr<Dictionary> markerDictionary = getPredefinedDictionary(DICT_4X4_50);	//using small dictionary for fast results
-																				
-	VideoCapture cap(CAMERA_INDEX);												//attempt to open the capture device. return if failure
-	if (!cap.isOpened())
+	//draw markers that are found
+	if (markerIDs.size() > 0)
 	{
-		cout << "Could not open camera " << CAMERA_INDEX << endl;
-		return -1;
+		drawDetectedMarkers(frame, markerCorners, markerIDs);
+		cout << "Found IDs:" << endl;
+		cout << Mat(markerIDs) << endl;
 	}
 
-	//keep the video stream running until we press a key
-	while (true)
+	//track the robot
+	calculateMarkerCenterAndAngle(markerIDs, markerCorners, markerCenters, markerThetas);
+	robotIndex = findRobotIndex(markerIDs);
+	if (robotIndex >= 0)//check if the robot is in the frame, draw its path if so.
 	{
-		cap >> grabbed;//capture frame from camera
-
-		if (grabbed.empty())//check if captured frame was valid, try again if not.
-		{
-			cout << "Invalid frame caught!" << endl;
-			continue;
-		}
-
-		undistort(grabbed, frame, cameraMatrix, distortionCoeffs);//correct lens distortion on captured frame
-
-		detectMarkers(frame, markerDictionary, markerCorners, markerIDs, parameters);
-
-		//draw markers that are found
-		if (markerIDs.size() > 0)
-		{
-			drawDetectedMarkers(frame, markerCorners, markerIDs);
-			cout << "Found IDs:" << endl;
-			cout << Mat(markerIDs) << endl;
-		}
-
-		//track the robot
-		calculateMarkerCenterAndAngle(markerIDs, markerCorners, markerCenters, markerThetas);
-		robotIndex = findRobotIndex(markerIDs);//check if the robot is in the frame, draw its path if so.
-		if (robotIndex >= 0)
-		{
-			drawPath(frame, markerCenters[robotIndex], robotLocationsBuffer);
-		}
-
-		//track the ball
-		circleCenter = trackCircles(frame, cannyUpperThreshValue, centerDetectionThreshValue, minBallRadiusValue, maxBallRadiusValue, blurKernelRadiusValue);
-		if (circleCenter != Point2f(-1, -1))
-		{
-			drawPath(frame, circleCenter, ballLocationsBuffer);
-		}
-
-		//draw the field
-		fieldCorners = drawFieldCorners(frame, markerIDs, markerCenters);
-		if (fieldCorners.size() > 1)//print only if two corners were found
-		{
-			cout << "Field corners found = " << endl << fieldCorners << endl;
-		}
-		
-		resize(frame, frame, Size(960, 720));
-		imshow(CAMERA_WINDOW_NAME, frame);
-
-		char keypress = waitKey(WAITKEY_DELAY_MS);
-		if(keypress > 0)
-		{
-			break;
-		}
+		robotLocation = markerCenters[robotIndex];
+		robotOrientation = markerThetas[robotIndex];
+		drawPath(frame, robotLocation, robotLocationsBuffer, Scalar(0,0,255));
 	}
-	return 0;//success
+	else
+	{
+		robotLocation = Point2f(-1, -1);//robot not found in frame
+		robotOrientation = 0.0f;
+	}
+
+	//track the ball
+	ballLocation = trackCircles(frame, gray, cannyUpperThreshValue, centerDetectionThreshValue, minBallRadiusValue, maxBallRadiusValue, blurKernelRadiusValue);
+	if (ballLocation != Point2f(-1, -1))
+	{
+		drawPath(frame, ballLocation, ballLocationsBuffer, Scalar(255, 0, 0));
+	}
+
+	//draw the field
+	fieldCorners = drawFieldCorners(frame, markerIDs, markerCenters);
+	if (fieldCorners.size() > 1)//print only if two corners were found
+	{
+		cout << "Field corners found = " << endl << fieldCorners << endl;
+	}
+	return frame;
+
 }
 
 //reads calibration file and loads camera matrix and distortion vector data
@@ -268,25 +250,35 @@ int findRobotIndex(vector<int> markerIDs)
 }
 
 //draws the path of a tracked object on the frame using a fifo buffer of previous points
-void drawPath(Mat frame, Point2f newLocation, deque<Point2f> &locationsBuffer)
+void drawPath(Mat &frame, Point2f newLocation, deque<Point2f> &locationsBuffer, Scalar lineColor)
 {
+	int baseColor[3] = { lineColor[0], lineColor[1], lineColor[2] };
+	int segmentShader[3] = { 0,0,0 };
 	locationsBuffer.push_back(newLocation);//add newest item to list
 	if (locationsBuffer.size() > MAX_PATH_BUFFER_LENGTH)
 	{
 		locationsBuffer.pop_front();//start removing oldest items
 		for (int i = 0; i < (locationsBuffer.size() - 1); i++)//buffer size should already be >= 2
 		{
-			line(frame, locationsBuffer[i], locationsBuffer[i + 1], Scalar(0,0,((64*i) % 256)), 3);
+			for (int j = 0; j < 3; j++)
+			{
+				if (baseColor[j] > 0)
+				{
+					segmentShader[j] = (baseColor[j] + i * 64) % 256;
+				}
+			}
+			Scalar segmentColor = Scalar(segmentShader[0], segmentShader[1], segmentShader[2]);
+			line(frame, locationsBuffer[i], locationsBuffer[i + 1], segmentColor, 3);
+			//cout << segmentColor << endl;
 		}
 	}
 }
 
 
-Point2f trackCircles(Mat frame, int &cannyUpperThreshValue,
+Point2f trackCircles(Mat &frame, Mat &gray, int &cannyUpperThreshValue,
 	int &centerDetectionThreshValue, int &minBallRadiusValue, int &maxBallRadiusValue, int &blurKernelRadiusValue)
 {
 	vector<Vec3f> circles;
-	Mat gray;
 	cvtColor(frame, gray, COLOR_BGR2GRAY);
 	medianBlur(gray, gray, blurKernelRadiusValue * 2 + 1);//apply a blur to the gray frame to help find circles
 
@@ -298,7 +290,6 @@ Point2f trackCircles(Mat frame, int &cannyUpperThreshValue,
 	{
 		Point2f c = { circles[0][0], circles[0][1] };
 		circle(gray, Point(c.x, c.y), circles[0][2], Scalar(0, 0, 255), 3, LINE_AA);
-		imshow("blurred grayscale", gray);
 		circle(frame, Point(c.x, c.y), 2, Scalar(0, 255, 0), 3, LINE_AA);
 		cout << "Ball found @ : " << c << endl;
 		return c;
@@ -313,8 +304,52 @@ int main()
 {
 	Mat cameraMatrix(3, 3, CV_64F);
 	Mat distortionCoefficients(1,5, CV_64F);
+	Mat grabbedFrame, processedFrame;
+	Point2f ballLocation, robotLocation;
+	float robotOrientation;
+	vector<Point2f> fieldCorners;
+	deque<Point2f> robotLocationsBuffer, ballLocationsBuffer;
+	Ptr<DetectorParameters> parameters = DetectorParameters::create();			//using default detector parameters for now
+	Ptr<Dictionary> markerDictionary = getPredefinedDictionary(DICT_4X4_50);	//using small dictionary for fast results
+
 	readCameraCalibration(CALIB_CONSTANTS_FILEPATH, cameraMatrix, distortionCoefficients);
-	startWebcamMonitoring(cameraMatrix, distortionCoefficients, ARUCO_SQUARE_LENGTH);
+
+	
+
+	VideoCapture cap(CAMERA_INDEX);												//attempt to open the capture device. return if failure
+	if (!cap.isOpened())
+	{
+		cout << "Could not open camera " << CAMERA_INDEX << endl;
+		return -1;
+	}
+
+	//keep the video stream/tracking running until we press a key
+	while (true)
+	{
+		cap >> grabbedFrame;//capture frame from camera
+
+		if (grabbedFrame.empty())//check if captured frame was valid, try again if not.
+		{
+			cout << "Invalid frame caught!" << endl;
+			continue;
+		}
+
+		processedFrame = trackObjects(grabbedFrame, parameters, cameraMatrix, distortionCoefficients, markerDictionary, ballLocation, robotLocation
+			, robotOrientation, fieldCorners, ballLocationsBuffer, robotLocationsBuffer);//does the actual ball/robot tracking
+		
+		resize(processedFrame, processedFrame, Size(960, 720));
+		imshow(CAMERA_WINDOW_NAME, processedFrame);
+
+		char keypress = waitKey(WAITKEY_DELAY_MS);
+		if (keypress > 0)
+		{
+			break;
+		}
+
+		
+	}
+
+
     return 0;
 }
 
